37c150ea2bae0a579f60667284d398d7f8e0da21
jinooLi <jinu1423@naver.com>
2024-10-07 12:44:26 +0000

    Add .sh file to use ros2 commands easier

---
+++ scripts/commands_setup.sh
@@ -0,0 +1,46 @@
+#!bin/bash
+
+# ROS2 setup
+. /opt/ros/foxy/setup.bash
+
+if [ -f '/workspaces/test_ws/install/setup.bash' ]; then
+    . /workspaces/test_ws/install/setup.bash
+
+    # ROS2 package commands
+    alias run_imu="ros2 launch stella_ahrs stella_ahrs_launch.py"
+    alias run_lidar=" ros2 launch urg_node2 urg_node2.launch.py"
+    alias run_vesc_driver="ros2 launch vesc_driver vesc_driver_node.launch.py"
+    alias run_vesc_ackermann="ros2 launch vesc_ackermann ackermann_to_vesc_node.launch.xml"
+    alias run_joy="ros2 run joy joy_node"
+    alias run_car_control="ros2 run car_control car_control"
+
+    command_list=(
+        'run_imu'
+        'run_lidar'
+        'run_vesc_driver'
+        'run_vesc_ackermann'
+        'run_joy'
+        )
+
+    run_commands() {
+        for sim_command in "${command_list[@]}"; do
+            ${sim_command} > /dev/null 2>&1 # background로 실행 후 출력을 무시
+            sleep .5 # 각 명령어 실행 후 0.5초 대기
+        done
+    }
+
+    save_commands_pid() { # job id를 저장하기 위해 job_id_list에 저장
+        job_id_list=($(jobs -l | grep sim_command | awk '{print $1}'))
+    }
+
+    kill_commands() {
+        for job_id in "${job_id_list[@]}"; do
+            kill %${job_id:1:-2} # job id만을 얻기 위해 job_id의 앞 뒤 두 글자를 제외함
+        done
+        jobs -l | grep sim_command
+    }
+
+    alias run_all_devices='run_commands && save_commands_pid'
+
+    alias stop_all_devices='kill_commands'
+fi
