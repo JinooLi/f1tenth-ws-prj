00d3e9c17fce40e7e50c3e428367d355773b4639
jinoo <jinu1423@naver.com>
2024-10-08 16:51:11 +0000

    hotfix: drop broken submodules

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,7 @@ cd `dirname $0` #
 
 # Register the commands_setup.sh file as an alias in bashrc
 echo "alias rosinit=\". /workspaces/test_ws/scripts/commands_setup.sh\"" >> ~/.bashrc
+echo "rosinit" >> ~/.bashrc
 
 # go to workspace
 cd ..
@@ -15,3 +16,7 @@ cd ..
 # --rosdistro foxy: Specifies the ROS 2 distribution to use (in this case, Foxy).
 # -y: Automatically confirms the installation of dependencies without prompting the user.
 rosdep install -i --from-path src --rosdistro foxy -y
+
+# Install f1tenth_gym 
+cd src/simulation/f1tenth_gym
+pip3 install -e .
